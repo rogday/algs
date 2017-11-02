a685d755fa8d3a6ee9769f0a76f51356cf840859
rogday <s.e.a.98@yandex.ru>
2017-11-02 20:26:08 +0300

    upld

---
+++ dream.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+void count_sort(vector<int>& vec) {
+	vector<int> tmp(1001);
+
+	for (auto& i : vec)
+		tmp[i]++;
+
+	for (int i = 0, k = 0; i < 1001; ++i) {
+		fill_n(vec.begin() + k, tmp[i], i);
+		k += tmp[i];
+	}
+}
+
+int main() {
+	istream_iterator<int> inst(cin);
+	int n = *(inst++), k = *(inst++);
+
+	vector<int> vec;
+	copy_n(inst, n, back_inserter(vec));
+	count_sort(vec);
+
+	int msum = 0, sum;
+
+	for (int i = 0; i < n - 2; ++i)	{
+		sum = vec[i] + vec[i + 1];
+
+		for (int j = i + 2; (j < n) && (vec[j] <= vec[i] + vec[i + 1] + k); ++j)
+			sum += vec[j];
+
+		if (sum > msum)
+			msum = sum;
+	}
+
+	cout << msum << endl;
+}
