81c756282ae268a4fa774b37a8e93a391bc77cee
rogday <s.e.a.98@yandex.ru>
2017-12-07 15:01:58 +0300

    speeded up

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <chrono>
-
-using namespace std;
 
 int main() {
 	int warr[31], parr[31];
 	int n, w, mp = 0, tw, tp;
-	cin >> n >> w;
+	std::cin >> n >> w;
 
-	for (int i = 0; i < n; ++i) cin >> warr[i] >> parr[i];
-
-	auto start = std::chrono::system_clock::now();
+	for (int i = 0; i < n; ++i) std::cin >> warr[i] >> parr[i];
 
 	for (int i = 1; i < (1 << n); ++i) {
 		tw = tp = 0;
@@ -22,9 +17,5 @@ int main() {
 		if (w >= tw && mp < tp) mp = tp;
 	}
 
-  	auto end = std::chrono::system_clock::now();
-
-  	std::chrono::duration<double> elapsed_seconds = end-start;
-
-	cout << "\nPrice: " << mp << "\nTime elapsed: " << elapsed_seconds.count() << " sec.\n";
+	std::cout << "\nPrice: " << mp << '\n';
 }
