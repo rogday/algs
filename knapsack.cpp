cb35673ce516b79b8cf016d1dbeea125136d8122
rogday <s.e.a.98@yandex.ru>
2017-12-08 22:46:32 +0300

    atom test

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 int main() {
-<<<<<<< HEAD
   int warr[31], parr[31];
   int n, w, mp = 0, tw, tp;
   std::cin >> n >> w;
@@ -21,23 +20,4 @@ int main() {
   }
 
   std::cout << "\nPrice: " << mp << '\n';
-=======
-	int warr[31], parr[31];
-	int n, w, mp = 0, tw, tp;
-	std::cin >> n >> w;
-
-	for (int i = 0; i < n; ++i) std::cin >> warr[i] >> parr[i];
-
-	for (int i = 1; i < (1 << n); ++i) {
-		tw = tp = 0;
-		for (int k = 0; k < n; ++k)
-			if (i & (1 << k)){
-				tw += warr[k];
-				tp += parr[k];
-			};
-		if (w >= tw && mp < tp) mp = tp;
-	}
-
-	std::cout << "\nPrice: " << mp << '\n';
->>>>>>> 81c756282ae268a4fa774b37a8e93a391bc77cee
 }
