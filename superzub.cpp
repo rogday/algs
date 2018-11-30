705ff8fa4e3b49aba2a667aa0a958df5121b03c3
rogday <s.e.a.98@yandex.ru>
2018-12-01 00:33:09 +0300

    removed dirty code

--- superzub.cpp
+++ superzub.cpp
@@ -4,171 +4,166 @@
 #include <stack>
 #include <unordered_map>
 
-using namespace std;
-typedef unsigned int uint;
+using uint = unsigned int;
 
 inline uint top(uint num) {
-  int pos = num >> 27;
+	int pos = num >> 27;
 
-  if (pos > 5)
-    return num;
+	if (pos > 5)
+		return num;
 
-  pos += 3;
-  uint digit = (num & (0b111 << (pos * 3))) >> 9;
-  num &= ~(0b111 << (pos * 3));
+	pos += 3;
+	uint digit = (num & (0b111 << (pos * 3))) >> 9;
+	num &= ~(0b111 << (pos * 3));
 
-  num |= digit;
-  num += 3 << 27;
+	num |= digit;
+	num += 3 << 27;
 
-  return num;
+	return num;
 }
 
 inline uint bot(uint num) {
-  int pos = num >> 27;
+	int pos = num >> 27;
 
-  if (pos < 3)
-    return num;
+	if (pos < 3)
+		return num;
 
-  pos -= 3;
-  uint digit = (num & (0b111 << (pos * 3))) << 9;
-  num &= ~(0b111 << (pos * 3));
+	pos -= 3;
+	uint digit = (num & (0b111 << (pos * 3))) << 9;
+	num &= ~(0b111 << (pos * 3));
 
-  num |= digit;
-  num -= 3 << 27;
+	num |= digit;
+	num -= 3 << 27;
 
-  return num;
+	return num;
 }
 
 inline uint left(uint num) {
-  int pos = num >> 27;
+	int pos = num >> 27;
 
-  if (pos % 3 == 2)
-    return num;
+	if (pos % 3 == 2)
+		return num;
 
-  ++pos;
-  uint digit = (num & (0b111 << (pos * 3))) >> 3;
-  num &= ~(0b111 << (pos * 3));
+	++pos;
+	uint digit = (num & (0b111 << (pos * 3))) >> 3;
+	num &= ~(0b111 << (pos * 3));
 
-  num |= digit;
-  num += 1 << 27;
+	num |= digit;
+	num += 1 << 27;
 
-  return num;
+	return num;
 }
 
 inline uint right(uint num) {
-  int pos = num >> 27;
+	int pos = num >> 27;
 
-  if (pos % 3 == 0)
-    return num;
+	if (pos % 3 == 0)
+		return num;
 
-  --pos;
-  uint digit = (num & (0b111 << (pos * 3))) << 3;
-  num &= ~(0b111 << (pos * 3));
+	--pos;
+	uint digit = (num & (0b111 << (pos * 3))) << 3;
+	num &= ~(0b111 << (pos * 3));
 
-  num |= digit;
-  num -= 1 << 27;
+	num |= digit;
+	num -= 1 << 27;
 
-  return num;
+	return num;
 }
 
 void print(uint num) {
-  for (int i = 8; i >= 0; --i) {
-    cout << ((num & (0b111 << (i * 3))) >> (i * 3)) << ' ';
-    if (i % 3 == 0)
-      cout << endl;
-  }
-  cout << endl;
+	for (int i = 8; i >= 0; --i) {
+		std::cout << ((num & (0b111 << (i * 3))) >> (i * 3)) << ' ';
+		if (i % 3 == 0)
+			std::cout << std::endl;
+	}
+	std::cout << std::endl;
 }
 
-stack<uint> func(uint ians, uint iins) {
-  unordered_map<uint, void *> arr;
-  arr.reserve(181440);
+std::stack<uint> func(uint ians, uint iins) {
+	std::unordered_map<uint, uint> arr;
+	arr.reserve(362880); // 9!
 
-  stack<pair<const uint, void *> *> stk1;
-  stack<pair<const uint, void *> *> stk2;
+	std::stack<uint> stk1;
+	std::stack<uint> stk2;
 
-  pair<const uint, void *> *pCur;
-  pair<unordered_map<uint, void *>::iterator, bool> tmp;
+	uint pCur;
+	std::pair<std::unordered_map<uint, uint>::iterator, bool> tmp;
+	const std::array<uint (*)(uint), 4> pFunc = {top, bot, left, right};
 
-  arr[iins] = nullptr;
+	arr[iins] = iins;
+	stk1.push(iins);
 
-  auto tmp2 = arr.find(iins);
-  stk1.push(&(*tmp2));
+	do {
+		pCur = stk1.top();
+		stk1.pop();
 
-  const array<uint (*)(uint), 4> pFunc = {top, bot, left, right};
+		for (auto &func : pFunc) {
+			tmp = arr.try_emplace(func(pCur), pCur);
+			if (tmp.second)
+				stk2.push(tmp.first->first);
+		}
 
-  do {
-    pCur = stk1.top();
-    stk1.pop();
+		if (stk1.empty())
+			swap(stk1, stk2);
+	} while (pCur != ians);
 
-    for (auto &func : pFunc) {
-      tmp = arr.try_emplace(func(pCur->first), pCur);
-      if (tmp.second)
-        stk2.push(&(*tmp.first));
-    }
+	std::stack<uint> trace;
+	trace.push(pCur);
 
-    if (stk1.empty())
-      swap(stk1, stk2);
-  } while (pCur->first != ians);
+	while (pCur != arr[pCur]) {
+		pCur = arr[pCur];
+		trace.push(pCur);
+	}
 
-  stack<uint> trace;
-  trace.push(pCur->first);
-
-  while (pCur->second != nullptr) {
-    pCur = (pair<const unsigned int, void *> *)pCur->second;
-    trace.push(pCur->first);
-  }
-
-  return trace;
+	return trace;
 }
 
-uint convert(string &s) {
-  static unordered_map<char, uint> tmp;
-  uint m = 0, k = 0, pos = 0;
+uint convert(std::string &s) {
+	static std::unordered_map<char, uint> tmp;
+	uint m = 0, k = 0, pos = 0;
 
-  if (tmp.empty()) {
-    tmp[' '] = k++;
-    for (char &i : s)
-      if (tmp.try_emplace(i, k).second)
-        ++k;
-  }
+	if (tmp.empty()) {
+		tmp[' '] = k++;
+		for (char &i : s)
+			if (tmp.try_emplace(i, k).second)
+				++k;
+	}
 
-  k = 0;
+	k = 0;
+	int len = s.size();
 
-  int len = s.size();
+	for (int i = len - 1; i >= 0; --i) {
+		m += tmp[s[i]] << k;
+		k += 3;
+		if (s[i] == ' ')
+			pos = i;
+	}
 
-  for (int i = len - 1; i >= 0; --i) {
-    m += tmp[s[i]] << k;
-    k += 3;
-    if (s[i] == ' ')
-      pos = i;
-  }
+	m += (len - pos - 1) << 27;
 
-  m += (len - pos - 1) << 27;
-
-  return m;
+	return m;
 }
 
 int main() {
-  string instr = " buzrepus";
-  string anstr = "superzub ";
+	std::string instr = " buzrepus";
+	std::string anstr = "superzub ";
 
-  uint ians = convert(anstr);
-  uint iins = convert(instr);
+	uint ians = convert(anstr);
+	uint iins = convert(instr);
 
-  auto t1 = chrono::system_clock::now();
-  stack<uint> stk = func(ians, iins);
-  auto t2 = chrono::system_clock::now();
+	auto t1 = std::chrono::system_clock::now();
+	std::stack<uint> stk = func(ians, iins);
+	auto t2 = std::chrono::system_clock::now();
 
-  chrono::duration<double> elapsed = t2 - t1;
+	std::chrono::duration<double> elapsed = t2 - t1;
 
-  cout << stk.size() << " turns" << endl << endl;
-  while (!stk.empty()) {
-    print(stk.top());
-    stk.pop();
-  }
+	std::cout << stk.size() << " turns" << std::endl << std::endl;
+	while (!stk.empty()) {
+		print(stk.top());
+		stk.pop();
+	}
 
-  cout << "elapsed: " << elapsed.count() << " sec." << endl;
+	std::cout << "elapsed: " << elapsed.count() << " sec." << std::endl;
 
-  return 0;
+	return 0;
 }
